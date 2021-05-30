6db3b7341589b85bbc3ad2b4342fa24229a128fd
BrueShavrk <borisshavrin@yandex.ru>
2021-05-31 00:24:29 +0300

    added loading Dynamic Content in controller 'products' out json file

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+import json
+
 # controllers = views = функции
 
 
@@ -14,39 +16,11 @@ def index(request):
 def products(request):
     context = {
         'title': 'GeekShop',
-        'products_list': {
-            'item_1': {
-                'name': 'Худи черного цвета с монограммами adidas Originals',
-                'price': 6090,
-                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
-            },
-            'item_2': {
-                'name': 'Синяя куртка The North Face',
-                'price': 23725,
-                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'
-            },
-            'item_3': {
-                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-                'price': 3390,
-                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.'
-            },
-            'item_4': {
-                'name': 'Черный рюкзак Nike Heritage',
-                'price': 2340,
-                'description': 'Плотная ткань. Легкий материал.'
-            },
-            'item_5': {
-                'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
-                'price': 13590,
-                'description': 'Гладкий кожаный верх. Натуральный материал.'
-            },
-            'item_6': {
-                'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
-                'price': 2890,
-                'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.'
-            }
-        },
     }
+    with open('products/fixtures/products_list.json', encoding='UTF-8') as file:
+        products_dict = json.load(file)
+        for item, desc in products_dict.items():
+            context[item] = desc
     return render(request, 'products/products.html', context)
 
 
