df09ed906f47d10f335329e758870811e0f5030e
BrueShavrk <borisshavrin@yandex.ru>
2021-06-09 21:50:36 +0300

    final steps (added logout)

--- users/views.py
+++ users/views.py
@@ -31,3 +31,8 @@ def register(request):
         form = UserRegisterForm()
     context = {'title': 'GeekShop - Регистрация', 'form': form}
     return render(request, 'users/register.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
