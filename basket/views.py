f0a6f2b33edd5e62e2dd44c78e8236325954af0f
BrueShavrk <borisshavrin@yandex.ru>
2021-06-12 23:06:50 +0300

    added basket_remove() controller for basket

--- basket/views.py
+++ basket/views.py
@@ -10,9 +10,14 @@ def basket_add(request, product_id):
 
     if not basket.exists():
         Basket.objects.create(user=request.user, product=product, quantity=1)
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
     else:
         basket = basket.first()
         basket.quantity += 1
         basket.save()
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def basket_remove(request, id):
+    Basket.objects.get(id=id).delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
