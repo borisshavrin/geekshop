77f652f275c3d251d0add36df5f8ee4427bc1612
BrueShavrk <borisshavrin@yandex.ru>
2021-06-16 23:59:52 +0300

    deleted class="basket_list" in basket.html, because we have a class="col-lg-5" in profile.html

--- static/js/basket.js
+++ static/js/basket.js
@@ -1,12 +1,12 @@
 window.onload = function () {
-    $('.basket_list').on('click', 'input[type="number"]', function () {
+    $('.col-lg-5').on('click', 'input[type="number"]', function () {
         var t_href = event.target;
         var name = t_href.name          // item.id
         var value = t_href.value;       // item.quantity
         $.ajax({
             url: '/basket/edit/' + name + '/' + value + '/',
             success: function (data) {
-                $('.basket_list').html(data.result);        // отрисовка страницы с полученными данными
+                $('.col-lg-5').html(data.result);        // отрисовка страницы с полученными данными
             }
         });
         event.preventDefault();
