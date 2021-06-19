a43dde1744d0a89a7cb1409b650ed14a78d76de6
BrueShavrk <borisshavrin@yandex.ru>
2021-06-20 00:49:51 +0300

    added CREATE for admin users and added handles of messages/errors to admins templates

--- admins/views.py
+++ admins/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth, messages
+from django.urls import reverse
 
+from admins.forms import UserAdminRegisterForm
 from users.models import User
 
 
@@ -9,12 +12,22 @@ def index(request):
 
 # CRUD
 def admin_users(request):
-    context = {'users': User.objects.all()}
+    context = {'title': 'GeekShop - Админ | Пользователи',
+               'users': User.objects.all()}
     return render(request, 'admins/admin-users-read.html', context)
 
 
 def admin_users_create(request):
-    return render(request, 'admins/admin-users-create.html')
+    if request.method == 'POST':
+        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегистрировались!')
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminRegisterForm()
+    context = {'title': 'GeekShop - Админ | Регистрация', 'form': form}
+    return render(request, 'admins/admin-users-create.html', context)
 
 
 def admin_users_update(request, id):
