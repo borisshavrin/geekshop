357d4555d68a8e99f83a3e98af833160daa45da2
BrueShavrk <borisshavrin@yandex.ru>
2021-06-23 23:08:58 +0300

    added DeleteView with overwrite delete() method for admins app

--- admins/urls.py
+++ admins/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from admins.views import index, AdminUsersListView, AdminUsersUpdateView, admin_users_delete, AdminUsersCreateView, admin_users_return
+from admins.views import index, AdminUsersListView, AdminUsersUpdateView, AdminUsersDeleteView, AdminUsersCreateView, admin_users_return
 
 app_name = 'admins'
 
@@ -9,6 +9,6 @@ urlpatterns = [
     path('users/', AdminUsersListView.as_view(), name='admin_users'),
     path('users/create/', AdminUsersCreateView.as_view(), name='admin_users_create'),
     path('users/update/<int:pk>', AdminUsersUpdateView.as_view(), name='admin_users_update'),
-    path('users/delete/<int:id>', admin_users_delete, name='admin_users_delete'),
+    path('users/delete/<int:pk>', AdminUsersDeleteView.as_view(), name='admin_users_delete'),
     path('users/return/<int:id>', admin_users_return, name='admin_users_return'),
 ]
--- admins/views.py
+++ admins/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
 from users.models import User
@@ -74,13 +74,25 @@ class AdminUsersUpdateView(UpdateView):
     success_url = reverse_lazy('admins:admin_users')
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_users_delete(request, id):
-    user = User.objects.get(id=id)
-    user.is_active = False
-    user.save()
-    messages.success(request, 'Пользователь деактивирован!')
-    return HttpResponseRedirect(reverse('admins:admin_users'))
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_users_delete(request, id):
+#     user = User.objects.get(id=id)
+#     user.is_active = False
+#     user.save()
+#     messages.success(request, 'Пользователь деактивирован!')
+#     return HttpResponseRedirect(reverse('admins:admin_users'))
+
+
+class AdminUsersDeleteView(DeleteView):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    success_url = reverse_lazy('admins:admin_users')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.is_active = False
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
 
 @user_passes_test(lambda u: u.is_superuser)
