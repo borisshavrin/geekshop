ffb96f8c4a2328f1bc201a70cce5091c011c1313
BrueShavrk <borisshavrin@yandex.ru>
2021-06-23 22:38:32 +0300

    added UpdateView for admins app

--- admins/urls.py
+++ admins/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from admins.views import index, AdminUsersListView, admin_users_update, admin_users_delete, AdminUsersCreateView, admin_users_return
+from admins.views import index, AdminUsersListView, AdminUsersUpdateView, admin_users_delete, AdminUsersCreateView, admin_users_return
 
 app_name = 'admins'
 
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('users/', AdminUsersListView.as_view(), name='admin_users'),
     path('users/create/', AdminUsersCreateView.as_view(), name='admin_users_create'),
-    path('users/update/<int:id>', admin_users_update, name='admin_users_update'),
+    path('users/update/<int:pk>', AdminUsersUpdateView.as_view(), name='admin_users_update'),
     path('users/delete/<int:id>', admin_users_delete, name='admin_users_delete'),
     path('users/return/<int:id>', admin_users_return, name='admin_users_return'),
 ]
