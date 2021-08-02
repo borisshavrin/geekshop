8f433c72c1789cf7a42d15b0bab89d9bff5f526f
BrueShavrk <borisshavrin@yandex.ru>
2021-08-02 20:03:52 +0300

    added price field for orders form

--- orders/views.py
+++ orders/views.py
@@ -44,6 +44,7 @@ class OrderItemsCreateView(LoginRequiredMixin, CreateView):
                 for num, form in enumerate(formset.forms):
                     form.initial['product'] = basket_items[num].product
                     form.initial['quantity'] = basket_items[num].quantity
+                    form.initial['price'] = basket_items[num].product.price
 
                 basket_items.delete()
             else:
@@ -84,6 +85,9 @@ class OrderItemsUpdateView(LoginRequiredMixin, UpdateView):
             formset = OrderFormSet(self.request.POST, instance=self.object)
         else:
             formset = OrderFormSet(instance=self.object)
+            for form in formset.forms:
+                if form.instance.pk:
+                    form.initial['price'] = form.instance.product.price
 
         context['orderitems'] = formset
         return context
